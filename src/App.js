7e15902ba5197977625f2cce4c870dc72e1eb406
Sushant Singh <sushantr134@gmail.com>
2019-08-26 19:03:12 +0530

    Some issues are fixed related to UI

--- src/App.js
+++ src/App.js
@@ -1,100 +1,119 @@
-import React from 'react';
-import {Header} from './components/Header'
-import {Search} from './components/Search'
+import React from "react";
+import { Header } from "./components/Header";
+import { Search } from "./components/Search";
 
-import axios from 'axios';
-import './app.global.scss';
-import {AppContent} from "./components/AppContent";
-import {LandingPage} from "./components/LandingPage";
+import axios from "axios";
+import "./app.global.scss";
+import { AppContent } from "./components/AppContent";
+import { LandingPage } from "./components/LandingPage";
 
 class App extends React.PureComponent {
-    constructor(props) {
-        super(props);
-        this.state = {appData: [], searchText: "", isLoading: true, index: 1, height: 730, removeLandingPage:false};
-        this.loadContent = this.loadContent.bind(this);
-        this.handleSearch = this.handleSearch.bind(this);
-        this.updateSearch = this.updateSearch.bind(this);
-        this.loadMore = this.loadMore.bind(this);
-        this.iScroll = React.createRef();
-    }
+  constructor(props) {
+    super(props);
+    this.state = { appData: [], searchText: "", isLoading: true, index: 1, height: 730, removeLandingPage: false };
+    this.loadContent = this.loadContent.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+    this.updateSearch = this.updateSearch.bind(this);
+    this.loadMore = this.loadMore.bind(this);
+    this.iScroll = React.createRef();
+  }
 
-    loadMore() {
-        this.setState((prev) => {
-            return {
-                index: prev.index + 1
-            }
+  loadMore() {
+    this.setState(prev => {
+      return {
+        index: prev.index + 1
+      };
+    });
+  }
+
+  componentDidMount() {
+    console.log(this.iScroll);
+    if (this.iScroll.current !== undefined) {
+      console.log(this.iScroll);
+      this.iScroll.current.addEventListener("scroll", () => {
+        if (this.iScroll.current.scrollTop + this.iScroll.current.clientHeight >= this.iScroll.current.scrollHeight) {
+          this.loadMore();
+        }
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevState.index !== this.state.index) {
+      this.loadContent();
+    }
+  }
+
+  handleSearch = event => {
+    event.preventDefault();
+    this.setState({ searchText: event.target.value });
+  };
+
+  updateSearch = (event, newvalue) => {
+    event.preventDefault();
+    const newAppData = this.state.appData.filter(obj => obj.ingredients.includes(newvalue));
+    console.log(newAppData);
+    this.setState(prev => {
+      return {
+        searchText: prev.searchText + ", " + newvalue,
+        appData: [...prev.appData, ...newAppData]
+      };
+    });
+  };
+
+  loadContent = event => {
+    //event.preventDefault();
+    if (this.state.searchText.length > 0) {
+      const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+      let endpoint = `/api/?i=${this.state.searchText}&p=${this.state.index}`;
+      this.setState({ removeLandingPage: true });
+      if (process.env.REACT_APP_API_URL) {
+        endpoint = `${process.env.REACT_APP_API_URL}/api/?i=${this.state.searchText}&p=${this.state.index}`;
+      }
+      axios
+        .get(proxyUrl + endpoint)
+        .then(res => {
+          this.setState(prev => {
+            return { appData: [...prev.appData, ...res.data.results], isLoading: false };
+          });
         })
-    }
-
-    componentDidMount() {
-        console.log(this.iScroll);
-        if (this.iScroll.current !== undefined) {
-            console.log(this.iScroll);
-            this.iScroll.current.addEventListener("scroll", () => {
-                if (this.iScroll.current.scrollTop + this.iScroll.current.clientHeight >= this.iScroll.current.scrollHeight) {
-                    this.loadMore();
-                }
-            });
-        }
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        if (prevState.index !== this.state.index) {
-            this.loadContent();
-        }
-    }
-
-
-    handleSearch = (event) => {
-        event.preventDefault();
-        this.setState({searchText: event.target.value})
-    };
-
-    updateSearch = (event, newvalue) => {
-        event.preventDefault();
-        const newAppData = this.state.appData.filter((obj) => obj.ingredients.includes(newvalue));
-        console.log(newAppData);
-        this.setState((prev) => {
-            return {
-                searchText: prev.searchText + ", " + newvalue,
-                appData: [...prev.appData, ...newAppData]
-            }
+        .catch(err => {
+          console.log(err.message);
         });
-    };
-
-    loadContent = (event) => {
-        //event.preventDefault();
-        const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-        let endpoint = `/api/?i=${this.state.searchText}&p=${this.state.index}`;
-        this.setState({removeLandingPage:true});
-        if (process.env.REACT_APP_API_URL) {
-            endpoint = `${process.env.REACT_APP_API_URL}/api/?i=${this.state.searchText}&p=${this.state.index}`
-        }
-        axios.get(proxyUrl + endpoint).then((res) => {
-            this.setState(prev => {
-                return {appData: [...prev.appData, ...res.data.results], isLoading: false}
-            })
-        }).catch((err) => {
-            console.log(err.message);
-        })
-
-    };
-
-    render() {
-        const SearchPanel = ( <Search animate={this.state.removeLandingPage} defaultValue={this.state.searchText} onclickLoad={this.loadContent}
-                                      onChangeHandleSearch={this.handleSearch}/>);
-
-        return (
-            <div ref={this.iScroll} style={{height: "100vh", overflow: "auto"}}>
-                <Header text={"Recipe Search"} subText={"A search engine to find their recipes by the ingredients."}/>
-                <LandingPage animate={this.state.removeLandingPage} searchPanel={SearchPanel} />
-
-                <AppContent loadMore={this.loadMore} data={this.state.appData} updateSearch={this.updateSearch}
-                            isLoading={this.state.isLoading}/>
-            </div>
-        )
     }
+  };
 
+  render() {
+    const SearchPanel = (
+      <Search
+        animate={this.state.removeLandingPage}
+        defaultValue={this.state.searchText}
+        onclickLoad={this.loadContent}
+        onChangeHandleSearch={this.handleSearch}
+      />
+    );
+
+    return (
+      <div ref={this.iScroll} style={{ height: "100vh", overflow: "auto" }}>
+        <Header
+          text={"Recipe Search"}
+          animate={this.state.removeLandingPage}
+          subText={"A search engine to find their recipes by the ingredients."}>
+          {this.state.removeLandingPage && (
+            <Search
+              animate={false}
+              defaultValue={this.state.searchText}
+              onclickLoad={this.loadContent}
+              onChangeHandleSearch={this.handleSearch}
+            />
+          )}
+        </Header>
+        <LandingPage animate={this.state.removeLandingPage} searchPanel={SearchPanel} />
+
+        <AppContent loadMore={this.loadMore} data={this.state.appData} updateSearch={this.updateSearch} isLoading={this.state.isLoading} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,12 +1,14 @@
 import React from "react";
-import styles from './styles.module.scss';
+import styles from "./styles.module.scss";
 
-
-export const LandingPage = ({searchPanel,animate}) => {
-    return (
-        <div className={`${styles.landingContainer} ${animate && styles.animateContainer}`}>
-            {searchPanel}
-            <p className={`${animate && styles.animatePara}`}>Type in Some ingredients you like and we will suggest you nice dishes that use it.</p>
-        </div>
-    )
+export const LandingPage = ({ searchPanel, animate, children }) => {
+  return (
+    <div className={`${styles.landingContainer} ${animate && styles.animateContainer}`}>
+      {children}
+      {searchPanel}
+      <p className={`${animate && styles.animatePara}`}>
+        Type in Some ingredients you like and we will suggest you nice dishes that use it.
+      </p>
+    </div>
+  );
 };
